3bcf59bff44931a67f5e3aec594a9c2f73460854
Polo <44245224+polkowskipawel@users.noreply.github.com>
2018-10-26 18:53:38 +0200

    Update main.java

--- matrix/main.java
+++ matrix/main.java
@@ -1,13 +1,12 @@
+//You are given an m x n 2D image matrix where each integer represents a pixel. Flip it in-place along its horizontal axis.
 public static void flipHorizontalAxis(int[][] matrix) {
   
   int pom = 0,i=0,j=0;
   int licz =0;
   
-  j = matrix.length-1;
+  j = matrix.length-1;                              //wielkosc tablicy matrix
        
-
         for(i = 0 ; i< j ; i++ ){
-
             for(licz = 0;licz < matrix[0].length; licz++){
                     pom = matrix[i][licz];
                     matrix[i][licz] = matrix[j][licz];
